46e4118a7a5df7b60bf3cc34669bab5c3ce9cd21
Serega007 <ermaksochi@gmail.com>
2021-01-09 10:19:31 +0300

    Откуда ты здесь взялось

--- scripts/spaceengineers.js
+++
@@ -1,5 +0,0 @@
-//Пилюля от жадности
-document.getElementById('adblock-notice').style.display = 'none'
-document.getElementById('vote-loading-block').style.display = 'none'
-document.getElementById('vote-form-block').removeAttribute('style')
-document.getElementById('blocked-notice').style.display = 'none'
