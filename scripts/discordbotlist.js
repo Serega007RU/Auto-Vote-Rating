3526d1596bc1dbe448ad12f8e9753bfec2231dfb
Serega007 <ermaksochi@gmail.com>
2021-02-09 14:10:22 +0300

    Исправление ошибок DiscordBotList

--- scripts/discordbotlist.js
+++ scripts/discordbotlist.js
@@ -35,30 +35,38 @@ function vote() {
         }
 
         this.check2 = setInterval(()=>{
-            if (document.querySelector('button[class="btn btn-blurple"]').disabled == false) {
-                document.querySelector('button[class="btn btn-blurple"]').click()
-                clearInterval(this.check2)
+            try {
+                if (document.querySelector('button.btn.btn-blurple').disabled == false) {
+                    document.querySelector('button.btn.btn-blurple').click()
+                    clearInterval(this.check2)
+                }
+            } catch (e) {
+                chrome.runtime.sendMessage({message: 'Ошибка! Кажется какой-то нужный элемент (кнопка или поле ввода) отсутствует. Вот что известно: ' + e.name + ': ' + e.message + '\n' + e.stack})
             }
         }, 1000)
         
         this.check = setInterval(()=>{
-            if (document.querySelector('div[role="status"][aria-live="polite"]').textContent == 'User has already voted.') {
-                chrome.runtime.sendMessage({later: true})
-                clearInterval(this.check)
-            } else if (document.querySelector('div[class="col-12 col-md-6 text-center"] > h1').textContent == 'Thank you for voting!') {
-                chrome.runtime.sendMessage({successfully: true})
-                clearInterval(this.check)
-            } else if (document.querySelector('div[role="status"][aria-live="polite"]').textContent != '') {
-                chrome.runtime.sendMessage({message: document.querySelector('div[role="status"][aria-live="polite"]').textContent})
-                clearInterval(this.check)
-            } else {
-                for (const el of document.querySelectorAll('link')) {
-                    if (el.href.includes('thanks')) {
-                        chrome.runtime.sendMessage({successfully: true})
-                        clearInterval(this.check)
-                        break
+            try {
+                if (document.querySelector('div[role="status"][aria-live="polite"]').textContent == 'User has already voted.') {
+                    chrome.runtime.sendMessage({later: true})
+                    clearInterval(this.check)
+                } else if (document.querySelector('div[class="col-12 col-md-6 text-center"] > h1').textContent == 'Thank you for voting!') {
+                    chrome.runtime.sendMessage({successfully: true})
+                    clearInterval(this.check)
+                } else if (document.querySelector('div[role="status"][aria-live="polite"]').textContent != '') {
+                    chrome.runtime.sendMessage({message: document.querySelector('div[role="status"][aria-live="polite"]').textContent})
+                    clearInterval(this.check)
+                } else {
+                    for (const el of document.querySelectorAll('link')) {
+                        if (el.href.includes('thanks')) {
+                            chrome.runtime.sendMessage({successfully: true})
+                            clearInterval(this.check)
+                            break
+                        }
                     }
                 }
+            } catch (e) {
+                chrome.runtime.sendMessage({message: 'Ошибка! Кажется какой-то нужный элемент (кнопка или поле ввода) отсутствует. Вот что известно: ' + e.name + ': ' + e.message + '\n' + e.stack})
             }
         }, 1000)
 
