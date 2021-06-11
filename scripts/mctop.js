0e126c8af99b469b1a02c1f7dfacab7362f3704c
Serega007 <ermaksochi@gmail.com>
2021-06-11 12:34:41 +0300

    remove debug code

--- scripts/mctop.js
+++ scripts/mctop.js
@@ -6,7 +6,7 @@ document.addEventListener('DOMContentLoaded', (event)=>{
     script.remove()
 })
 
-async function vote(first, event) {
+async function vote(first) {
     if (first == true || first == false) return
     try {
         //Если погльзователь уже авторизован в вк, сразу голосует
@@ -37,24 +37,24 @@ async function vote(first, event) {
     }
 }
 
-// const timer = setInterval(()=>{
-//     try {
-//         //Ищет надпись в которой написано что вы проголосовали или вы уже голосовали, по этой надписи скрипт завершается
-//         if (document.readyState == 'complete' && document.querySelectorAll('div[class=tooltip-inner]').item(0) != null) {
-//             const textContent = document.querySelectorAll('div[class=tooltip-inner]').item(0).textContent
-//             if (textContent == 'Сегодня Вы уже голосовали с данного вк') {
-//                 chrome.runtime.sendMessage({later: 'vk_error'})
-//             } else if (textContent == 'Сегодня Вы уже голосовали с этим ником') {
-//                 chrome.runtime.sendMessage({later: 'nick_error'})
-//             } else if (textContent == 'Спасибо за Ваш голос, Вы сможете повторно проголосовать завтра.') {
-//                 chrome.runtime.sendMessage({successfully: true})
-//             } else {
-//                 chrome.runtime.sendMessage({message: textContent})
-//             }
-//             clearInterval(timer)
-//         }
-//     } catch (e) {
-//         throwError(e)
-//         clearInterval(timer)
-//     }
-// }, 1000)
+const timer = setInterval(()=>{
+    try {
+        //Ищет надпись в которой написано что вы проголосовали или вы уже голосовали, по этой надписи скрипт завершается
+        if (document.readyState == 'complete' && document.querySelectorAll('div[class=tooltip-inner]').item(0) != null) {
+            const textContent = document.querySelectorAll('div[class=tooltip-inner]').item(0).textContent
+            if (textContent == 'Сегодня Вы уже голосовали с данного вк') {
+                chrome.runtime.sendMessage({later: 'vk_error'})
+            } else if (textContent == 'Сегодня Вы уже голосовали с этим ником') {
+                chrome.runtime.sendMessage({later: 'nick_error'})
+            } else if (textContent == 'Спасибо за Ваш голос, Вы сможете повторно проголосовать завтра.') {
+                chrome.runtime.sendMessage({successfully: true})
+            } else {
+                chrome.runtime.sendMessage({message: textContent})
+            }
+            clearInterval(timer)
+        }
+    } catch (e) {
+        throwError(e)
+        clearInterval(timer)
+    }
+}, 1000)
