a006c09443ed5045fdac428ad8379635b499489b
Serega007 <ermaksochi@gmail.com>
2021-06-11 14:20:22 +0300

    Неудачный 79084b421e9b050ef0e8ad1867fcd21579efbb7a возвращение, причина executeScript происходит раньше чем iframe загружается

--- background.js
+++ background.js
@@ -420,7 +420,7 @@ async function initializeConfig() {
     if (settings && !settings.disabledCheckTime) checkTime()
     
     //Да да, целую минуту ждём перед запуском расширения, больше никак не понять когда закончилась синхронизация браузера
-    if (storageArea == 'sync') await wait(60000)
+//  if (storageArea == 'sync') await wait(60000)
 
     //Проверка на голосование
     setInterval(async()=>{
@@ -1156,15 +1156,10 @@ async function checkResponseError(project, response, url, bypassCodes, vk) {
 }
 
 //Слушатель на обновление вкладок, если вкладка полностью загрузилась, загружает туда скрипт который сам нажимает кнопку проголосовать
-chrome.webRequest.onCompleted.addListener(function(details) {
+chrome.webNavigation.onCompleted.addListener(function(details) {
     let project = openedProjects.get(details.tabId)
     if (project == null) return
-    if (details.frameId == 0 && details.type == 'main_frame') {
-        if (details.statusCode < 200 || details.statusCode > 299) {
-            const sender = {tab: {id: details.tabId}}
-            endVote({errorVote: String(details.statusCode)}, sender, project)
-            return
-        }
+    if (details.frameId == 0) {
         chrome.tabs.executeScript(details.tabId, {file: 'scripts/' + getProjectName(project).toLowerCase() +'.js'}, function() {
             if (chrome.runtime.lastError) {
                 console.error(getProjectPrefix(project, true) + chrome.runtime.lastError.message)
@@ -1176,7 +1171,7 @@ chrome.webRequest.onCompleted.addListener(function(details) {
             }
         })
         chrome.tabs.executeScript(details.tabId, {file: 'scripts/api.js'})
-    } else if (details.frameId != 0 && details.type == 'sub_frame' && (details.url.match(/hcaptcha.com\/captcha\/*/) || details.url.match(/https:\/\/www.google.com\/recaptcha\/api.\/anchor*/) || details.url.match(/https:\/\/www.google.com\/recaptcha\/api.\/bframe*/) || details.url.match(/https:\/\/www.recaptcha.net\/recaptcha\/api.\/anchor*/) || details.url.match(/https:\/\/www.recaptcha.net\/recaptcha\/api.\/bframe*/))) {
+    } else if (details.frameId != 0 && (details.url.match(/hcaptcha.com\/captcha\/*/) || details.url.match(/https:\/\/www.google.com\/recaptcha\/api.\/anchor*/) || details.url.match(/https:\/\/www.google.com\/recaptcha\/api.\/bframe*/) || details.url.match(/https:\/\/www.recaptcha.net\/recaptcha\/api.\/anchor*/) || details.url.match(/https:\/\/www.recaptcha.net\/recaptcha\/api.\/bframe*/))) {
         chrome.tabs.executeScript(details.tabId, {file: 'scripts/captchaclicker.js', frameId: details.frameId}, function() {
             if (chrome.runtime.lastError) {
                 console.error(getProjectPrefix(project, true) + chrome.runtime.lastError.message)
@@ -1187,6 +1182,15 @@ chrome.webRequest.onCompleted.addListener(function(details) {
                 }
             }
         })
+    }
+}, {urls: ['<all_urls>']})
+
+chrome.webRequest.onCompleted.addListener(function(details) {
+    let project = openedProjects.get(details.tabId)
+    if (project == null) return
+    if (details.type == 'main_frame' && (details.statusCode < 200 || details.statusCode > 299)) {
+        const sender = {tab: {id: details.tabId}}
+        endVote({errorVote: String(details.statusCode)}, sender, project)
     }
 }, {urls: ['<all_urls>']})
 
