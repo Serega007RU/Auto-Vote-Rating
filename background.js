2cffa43ee137ae1bc00bba1a1b0cdfbf05088848
Serega007 <ermaksochi@gmail.com>
2021-08-05 11:18:02 +0300

    Исправление неверного регистра McTOP в рандомизации

--- background.js
+++ background.js
@@ -1023,8 +1023,8 @@ async function endVote(request, sender, project) {
 
         if (project.randomize) {
             project.time = project.time + Math.floor(Math.random() * 43200000)
-        } else if ((project.rating === 'TopCraft' || project.rating === 'McTop') && !project.priority) {
-            //Рандомизация по умолчанию (в пределах 5-ти минут) для бедного TopCraft/McTop который легко ддосится от массового автоматического голосования
+        } else if ((project.rating === 'TopCraft' || project.rating === 'McTOP') && !project.priority) {
+            //Рандомизация по умолчанию (в пределах 5-ти минут) для бедного TopCraft/McTOP который легко ддосится от массового автоматического голосования
             project.time = project.time + Math.floor(Math.random() * (300000 - -300000) + -300000)
         }
 
