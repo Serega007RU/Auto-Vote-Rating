e2f2cbc09a33ad58d5010135c3cd57bab9841ee5
Serega007 <ermaksochi@gmail.com>
2021-08-14 17:32:33 +0300

    net::ERR ошибки теперь определяются и на капчи (фреймы)

--- background.js
+++ background.js
@@ -827,17 +827,17 @@ chrome.webRequest.onErrorOccurred.addListener(function(details) {
     if (details.initiator === 'chrome-extension://' + chrome.runtime.id) {
         if (fetchProjects.has(details.requestId)) {
             let project = fetchProjects.get(details.requestId)
-//          if (details.error.includes('net::ERR_ABORTED') || details.error.includes('net::ERR_CONNECTION_RESET') || details.error.includes('net::ERR_CONNECTION_CLOSED') || details.error.includes('net::ERR_NETWORK_CHANGED')) {
-//              console.warn(getProjectPrefix(project, true) + details.error)
-//              return
-//          }
+            // if (details.error.includes('net::ERR_ABORTED') || details.error.includes('net::ERR_CONNECTION_RESET') || details.error.includes('net::ERR_CONNECTION_CLOSED') || details.error.includes('net::ERR_NETWORK_CHANGED') || details.error.includes('net::ERR_CACHE_MISS') || details.error.includes('net::ERR_BLOCKED_BY_CLIENT')) {
+            //     console.warn(getProjectPrefix(project, true) + details.error)
+            //     return
+            // }
             endVote({errorVoteNetwork: [details.error, details.url]}, null, project)
         }
-    } else if (details.type === 'main_frame') {
-        if (openedProjects.has(details.tabId)) {
+    } else if (openedProjects.has(details.tabId)) {
+        if (details.type === 'main_frame' || details.url.match(/hcaptcha.com\/captcha\/*/) || details.url.match(/https:\/\/www.google.com\/recaptcha\/api.\/anchor*/) || details.url.match(/https:\/\/www.google.com\/recaptcha\/api.\/bframe*/) || details.url.match(/https:\/\/www.recaptcha.net\/recaptcha\/api.\/anchor*/) || details.url.match(/https:\/\/www.recaptcha.net\/recaptcha\/api.\/bframe*/)) {
             let project = openedProjects.get(details.tabId)
-            if (details.error.includes('net::ERR_ABORTED') || details.error.includes('net::ERR_CONNECTION_RESET') || details.error.includes('net::ERR_CONNECTION_CLOSED') || details.error.includes('net::ERR_NETWORK_CHANGED')) {
-                console.warn(getProjectPrefix(project, true) + details.error)
+            if (details.error.includes('net::ERR_ABORTED') || details.error.includes('net::ERR_CONNECTION_RESET') || details.error.includes('net::ERR_CONNECTION_CLOSED') || details.error.includes('net::ERR_NETWORK_CHANGED') || details.error.includes('net::ERR_CACHE_MISS') || details.error.includes('net::ERR_BLOCKED_BY_CLIENT')) {
+                // console.warn(getProjectPrefix(project, true) + details.error)
                 return
             }
             const sender = {tab: {id: details.tabId}}
