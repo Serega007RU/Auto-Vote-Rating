a0be8446f452bc2894f779069d45dec94952902c
Serega007 <ermaksochi@gmail.com>
2021-08-18 17:06:57 +0300

    Костыли с Rocket Loader

--- scripts/mctop.js
+++ scripts/mctop.js
@@ -1,60 +1,63 @@
-//Совместимость с Rocket Loader и jQuery
-// document.addEventListener('DOMContentLoaded', ()=>{
-// //  const script = document.createElement('script')
-// //  script.textContent = `$(document).ready(function() {window.postMessage('voteReady', '*')})`
-// //  document.documentElement.appendChild(script)
-// //  script.remove()
-//     vote()
-// })
+//Костыли для Rocket Loader
+if (typeof loaded2 === 'undefined') {
+    // noinspection ES6ConvertVarToLetConst
+    var loaded2 = true
+    runVote()
+}
 
 async function vote(first) {
-    if (first) return
+    if (first === true) return
     try {
-        //Если погльзователь уже авторизован в вк, сразу голосует
-        if (document.querySelector('button[data-type=vote]') == null) {
-            //Клик 'Голосовать'
-            document.querySelector('button.btn.btn-info.btn-vote.openVoteModal').click()
-            //Вводит никнейм
-            const project = await getProject('McTOP')
-            document.querySelector('input[name=nick]').value = project.nick
-            //Клик 'Голосовать' в окне голосования
-            document.querySelector('button.btn.btn-info.btn-vote.voteBtn').click()
-        } else {
+        const project = await getProject('McTOP')
+        //Авторизованы ли мы в аккаунте?
+        if (!document.querySelector('#userLoginWrap').classList.contains('hidden')) {
             document.querySelector('button[data-type=vote]').click()
-            //Надо ли авторизовываться в вк, если не надо то сразу голосует
-            if (document.querySelector('#loginModal > div > div > div.modal-body > div > ul > li > a') != null) {
-                //Клик VK
-                document.querySelector('#loginModal > div > div > div.modal-body > div > ul > li > a').click()
-                clearInterval(timer)
-            } else {
-                const project = await getProject('McTOP')
-                document.querySelector('input[name=nick]').value = project.nick
-                document.querySelector('button.btn.btn-info.btn-vote.voteBtn').click()
-            }
+            document.querySelector('a.modalVkLogin').click()
+            return
         }
+        if (!document.querySelector('#voteModal').classList.contains('in')) {
+            document.querySelector('button.openVoteModal').click()
+        }
+        if (first || first == null) return
+        //Вводит никнейм
+        document.querySelector('input[name=nick]').value = project.nick
+        document.querySelector('input[name=nick]').click()
+        //Клик 'Голосовать' в окне голосования
+        document.querySelector('button.btn.btn-info.btn-vote.voteBtn').click()
     } catch (e) {
         throwError(e)
     }
 }
 
-const timer = setInterval(()=>{
-    try {
-        //Ищет надпись в которой написано что вы проголосовали или вы уже голосовали, по этой надписи скрипт завершается
-        if (document.readyState === 'complete' && document.querySelectorAll('div[class=tooltip-inner]').item(0) != null) {
-            const textContent = document.querySelectorAll('div[class=tooltip-inner]').item(0).textContent
-            if (textContent === 'Сегодня Вы уже голосовали с данного вк') {
-                chrome.runtime.sendMessage({later: 'vk_error'})
-            } else if (textContent === 'Сегодня Вы уже голосовали с этим ником') {
-                chrome.runtime.sendMessage({later: 'nick_error'})
-            } else if (textContent === 'Спасибо за Ваш голос, Вы сможете повторно проголосовать завтра.') {
-                chrome.runtime.sendMessage({successfully: true})
-            } else {
-                chrome.runtime.sendMessage({message: textContent})
+function runVote() {
+    //Совместимость с Rocket Loader и jQuery
+    document.addEventListener('DOMContentLoaded', ()=>{
+        // const script = document.createElement('script')
+        // script.textContent = `$(document).ready(function() {window.postMessage('voteReady', '*')})`
+        // document.documentElement.appendChild(script)
+        // script.remove()
+        vote()
+    })
+
+    const timer = setInterval(()=>{
+        try {
+            //Ищет надпись в которой написано что вы проголосовали или вы уже голосовали, по этой надписи скрипт завершается
+            if (document.readyState === 'complete' && document.querySelectorAll('div[class=tooltip-inner]').item(0) != null) {
+                const textContent = document.querySelectorAll('div[class=tooltip-inner]').item(0).textContent
+                if (textContent === 'Сегодня Вы уже голосовали с данного вк') {
+                    chrome.runtime.sendMessage({later: 'vk_error'})
+                } else if (textContent === 'Сегодня Вы уже голосовали с этим ником') {
+                    chrome.runtime.sendMessage({later: 'nick_error'})
+                } else if (textContent === 'Спасибо за Ваш голос, Вы сможете повторно проголосовать завтра.') {
+                    chrome.runtime.sendMessage({successfully: true})
+                } else {
+                    chrome.runtime.sendMessage({message: textContent})
+                }
+                clearInterval(timer)
             }
+        } catch (e) {
+            throwError(e)
             clearInterval(timer)
         }
-    } catch (e) {
-        throwError(e)
-        clearInterval(timer)
-    }
-}, 1000)
+    }, 1000)
+}
