9172810d55b7f16fb7870714cb92f92a6d8d22be
Serega007 <ermaksochi@gmail.com>
2021-09-10 18:09:45 +0300

    Исправление ошибок голосования на Top.GG связи с обновлением дизайна сайта (опять)

--- scripts/topgg.js
+++ scripts/topgg.js
@@ -21,74 +21,42 @@ async function vote(first) {
             return
         }
 
-        if (document.querySelector('div.modal.is-active') != null) {
-            if (document.querySelector('div.modal.is-active > div.modal-content.content').textContent.includes('You must be logged')) {
-                document.querySelector('div.modal.is-active > div.modal-content.content a.btn.primary').click()
-//              chrome.runtime.sendMessage({discordLogIn: true})
-                return
-            } else if (document.querySelector('div.modal.is-active a[class="btn"]') != null) {
-                document.querySelector('div.modal.is-active a[class="btn"]').click()
-            } else {
-                chrome.runtime.sendMessage({message: document.querySelector('div.modal.is-active > div.modal-content.content').textContent.trim()})
-                return
-            }
+        const login = findElement('button', ['login to vote'])
+        if (login != null) {
+            login.click()
+            return
         }
 
-        //Пилюля от жадности
-        if (document.getElementById('video-root')) document.getElementById('video-root').style.display = 'none'
-        if (document.getElementById('vote-root')) document.getElementById('vote-root').style.display = 'block'
-        if (document.querySelector('.slider-root')) document.querySelector('.slider-root').removeAttribute('style')
-        if (document.querySelector('#vote-button-container > a')) document.querySelector('#vote-button-container > a').click()
+        const vote = findElement('button', ['vote'])
+        vote.click()
 
-        //Ждём загрузки bot manager (а зачем нам это делать адекватно?)
-        await new Promise(resolve => {
-            const timer3 = setInterval(()=>{
-                if (document.getElementById('vote-label') != null) {
-                    if (document.getElementById('vote-label').textContent === 'Ready to vote') {
-                        resolve()
-                        clearInterval(timer3)
-                    }
-                }
-            }, 1000)
-        })
-
-        //Пилюля от жадности
-        if (document.getElementById('video-root')) document.getElementById('video-root').style.display = 'none'
-        if (document.getElementById('vote-root')) document.getElementById('vote-root').style.display = 'block'
-        if (document.querySelector('.slider-root')) document.querySelector('.slider-root').removeAttribute('style')
-        if (document.querySelector('#vote-button-container > a')) document.querySelector('#vote-button-container > a').click()
-        //Мы типо не роботы, мы человеки
-        for (let i = 0; i < 20; i++) {
-            triggerMouseEvent(document, 'mousedown')
-            triggerMouseEvent(document, 'mousemove')
-        }
-        function triggerMouseEvent(node, eventType) {
-            const clickEvent = document.createEvent('MouseEvents')
-            clickEvent.initEvent(eventType, true, true)
-            node.dispatchEvent(clickEvent)
-        }
-        document.querySelector('.vote-slider').click()
-
-        const timer2 = setInterval(()=>{
-            try {
-                const text = document.getElementById('vote-label').textContent
-                if (text.includes('already voted')) {
-                    chrome.runtime.sendMessage({later: true})
-                } else if (document.getElementById('reminder').textContent.includes('Thanks for voting!')) {
+        const timer1 = setInterval(()=>{
+            const result = findElement('p.chakra-text', ['thanks for voting', 'already voted', 'something went wrong'])
+            if (result != null) {
+                if (result.textContent.toLowerCase().includes('thanks for voting')) {
                     chrome.runtime.sendMessage({successfully: true})
-                } else if (text.includes('Ready to vote')) {
-                    return
+                } else if (result.parentElement.textContent.toLowerCase().includes('already voted')) {
+                    chrome.runtime.sendMessage({later: true})
+                } else if (result.textContent.toLowerCase().includes('already voted')) {
+                    chrome.runtime.sendMessage({later: true})
                 } else {
-                    chrome.runtime.sendMessage({message: text})
+                    chrome.runtime.sendMessage({message: result.parentElement.textContent})
                 }
-                clearInterval(timer2)
-            } catch (e) {
-                throwError(e)
-                clearInterval(timer2)
+                clearInterval(timer1)
             }
         }, 1000)
 
     } catch (e) {
         throwError(e)
+    }
+}
+
+function findElement(selector, text) {
+    for (const element of document.querySelectorAll(selector)) {
+        for (const t of text) {
+            if (element.textContent.toLowerCase().includes(t)) {
+                return element
+            }
+        }
     }
 }
