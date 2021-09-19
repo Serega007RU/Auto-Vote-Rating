3dd4a93813d4fe5dcf97f397234ad508ef4ad256
Serega007 <ermaksochi@gmail.com>
2021-09-20 02:01:34 +0300

    remove debug code

--- scripts/api.js
+++ scripts/api.js
@@ -9,7 +9,6 @@ if (typeof loaded === 'undefined') {
 }
 
 function run() {
-    return
     chrome.runtime.onMessage.addListener(function(request/*, sender, sendResponse*/) {
         if (request.sendProject) {
             proj = request.project
