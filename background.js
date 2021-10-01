a62492381e6f233c847ce2a93688397327d937c0
Serega007 <ermaksochi@gmail.com>
2021-10-01 09:16:05 +0300

    Перепутал название 1-ой переменной

--- background.js
+++ background.js
@@ -1834,7 +1834,7 @@ async function stopVote() {
 //Если требуется авторизация для Прокси
 let errorProxy = {ip: '', count: 0}
 chrome.webRequest.onAuthRequired.addListener(async function(details, callbackFn) {
-    if (details.isProxy && (currentProxy || settings.useProxyOnUnProxyTop)) {
+    if (details.isProxy && (currentProxy || settings.useProxyPacScript)) {
         if (errorProxy.ip !== currentProxy.ip) {
             errorProxy.count = 0
         }
