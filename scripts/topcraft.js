536eac8ea1d3bc98b7a0a81d66a275f1a163f466
Serega007 <ermaksochi@gmail.com>
2021-11-10 06:43:54 +0300

    Костыль костыля, дожидаемся загрузки "кривой" модалки входа через ВК

--- scripts/topcraft.js
+++ scripts/topcraft.js
@@ -23,7 +23,16 @@ async function vote(first) {
         //Авторизованы ли мы в аккаунте?
         if (!document.querySelector('#userLoginWrap').classList.contains('hidden')) {
             document.querySelector('button[data-type=vote]').click()
-            document.querySelector('a.modalVkLogin').click()
+            if (document.querySelector('a.modalVkLogin') == null) {
+                const timerLogin = setInterval(() => {
+                    if (document.querySelector('a.modalVkLogin') != null) {
+                        document.querySelector('a.modalVkLogin').click()
+                        clearInterval(timerLogin)
+                    }
+                }, 1000)
+            } else {
+                document.querySelector('a.modalVkLogin').click()
+            }
             return
         }
         if (!document.querySelector('#voteModal').classList.contains('in')) {
