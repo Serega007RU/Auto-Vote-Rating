881440556d1474437b4ad0c0c186c85a5f90c8f3
Serega007 <ermaksochi@gmail.com>
2022-01-05 01:43:07 +0300

    Исправление ошибки key already exists при обновлении версии настроек

--- main.js
+++ main.js
@@ -207,7 +207,7 @@ async function upgrade(db, oldVersion, newVersion, transaction) {
             lastSuccessVote: null,
             lastAttemptVote: null
         }
-        await other.add(todayStats, 'todayStats')
+        await other.put(todayStats, 'todayStats')
     }
 
     if (!generalStats) {
@@ -223,6 +223,6 @@ async function upgrade(db, oldVersion, newVersion, transaction) {
             lastAttemptVote: null,
             added: Date.now()
         }
-        await other.add(generalStats, 'generalStats')
+        await other.put(generalStats, 'generalStats')
     }
 }
