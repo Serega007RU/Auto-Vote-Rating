48d2b982e9a0c5fff67e76c0811bf276c7f8e493
Serega007 <ermaksochi@gmail.com>
2022-03-01 09:01:40 +0300

    Предусмотрено время следующего голосования при ошибке "Вы уже голосовали" на https://minecraftservers.org/

--- scripts/minecraftserversorg.js
+++ scripts/minecraftserversorg.js
@@ -19,7 +19,9 @@ const timer = setInterval(()=>{
     // }
     if (document.querySelector('#error-message') != null) {
         if (document.querySelector('#error-message').textContent.includes('already voted')) {
-            chrome.runtime.sendMessage({later: true})
+            const numbers = document.querySelector('#error-message').textContent.match(/\d+/g).map(Number)
+            const milliseconds = (numbers[0] * 60 * 60 * 1000) + (numbers[1] * 60 * 1000)/* + (sec * 1000)*/
+            chrome.runtime.sendMessage({later: Date.now() + milliseconds + 60000})
         } else if (document.querySelector('#error-message').textContent.includes('Thanks for voting')) {
             chrome.runtime.sendMessage({successfully: true})
         } else {
