ef40150cd4b9634e0792409e129cb3e53068e81f
Serega007 <ermaksochi@gmail.com>
2022-08-06 07:58:11 +0300

    Исправление ошибок на https://topminecraftservers.org/

--- scripts/topminecraftservers.js
+++ scripts/topminecraftservers.js
@@ -1,19 +1,21 @@
 async function vote(first) {
     if (first === false) return
     if (document.querySelector('div.alert.alert-danger') != null) {
-        const text = document.querySelector('div.alert.alert-danger').textContent
-        if (text.includes('already voted')) {
+        const text = document.querySelector('div.alert.alert-danger').textContent.toLowerCase()
+        if (text.includes('thanks for voting')) {
+            chrome.runtime.sendMessage({successfully: true})
+        } else if (text.includes('already voted')) {
             chrome.runtime.sendMessage({later: true})
         } else if (text.includes('captcha')) {
             chrome.runtime.sendMessage({errorCaptcha: text, restartVote: true})
         } else {
             chrome.runtime.sendMessage({message: text})
         }
-    } else if (document.querySelector('body > div.container > div > div > div > div.col-md-4 > button') != null) {
-        if (document.querySelector('body > div.container > div > div > div > div.col-md-4 > button').textContent.includes('already voted')) {
+    } else if (document.querySelector('div.row > div.col-md-4 > button')) {
+        if (document.querySelector('div.row > div.col-md-4 > button').textContent.includes('already voted') || document.querySelector('div.row > div.col-md-4 > button').textContent.toLowerCase().includes('thanks for voting')) {
             chrome.runtime.sendMessage({successfully: true})
         } else {
-            chrome.runtime.sendMessage({message: document.querySelector('body > div.container > div > div > div > div.col-md-4 > button').textContent})
+            chrome.runtime.sendMessage({message: document.querySelector('div.row > div.col-md-4 > button').textContent})
         }
     } else {
         //Ожидание загрузки reCATPCHA
