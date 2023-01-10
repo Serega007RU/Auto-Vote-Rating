5a0c1f0f11ae4d5cd5586f7354ac3087d841285d
Serega007 <ermaksochi@gmail.com>
2023-01-10 08:58:36 +0300

    https://www.minecraft-serverlist.net/ не отправляем репорт об ошибке если сервер больше не существует

--- scripts/minecraftserverlistnet.js
+++ scripts/minecraftserverlistnet.js
@@ -1,12 +1,17 @@
 async function vote(first) {
-    if (document.querySelector('div.alert.alert-danger') != null) {
-        if (document.querySelector('div.alert.alert-danger').textContent.includes('Du hast bereits')) {
+    if (document.querySelector('div.alert.alert-danger')) {
+        const request = {}
+        request.message = document.querySelector('div.alert.alert-danger').textContent
+        if (request.message.includes('Du hast bereits')) {
             const numbers = document.querySelector('div.alert.alert-danger').childNodes[2].textContent.match(/\d+/g).map(Number)
             const milliseconds = (numbers[0] * 60 * 60 * 1000) + (numbers[1] * 60 * 1000)/* + (sec * 1000)*/
             chrome.runtime.sendMessage({later: Date.now() + milliseconds})
             return
         }
-        chrome.runtime.sendMessage({message: document.querySelector('div.alert.alert-danger').textContent})
+        if (request.message.includes('Server existiert nicht')) {
+            request.ignoreReport = true
+        }
+        chrome.runtime.sendMessage(request)
         return
     }
     if (document.querySelector('div.alert.alert-success') != null) {
