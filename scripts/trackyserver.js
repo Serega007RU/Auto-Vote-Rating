e9c50d7302a8fe7c4ee4fb1b2d24b3db92688282
Serega007 <ermaksochi@gmail.com>
2023-03-15 11:21:10 +0300

    trackyserver.com не отправляем отчёт если ошибка "There was a problem during the vote"

--- scripts/trackyserver.js
+++ scripts/trackyserver.js
@@ -31,11 +31,16 @@ const timer = setInterval(()=>{
         }
         if (document.querySelector('div.alert.alert-danger') != null && document.querySelector('div.alert.alert-danger').parentElement.style.display !== 'none') {
             clearInterval(timer)
-            if (document.querySelector('div.alert.alert-danger').textContent.includes('already voted')) {
+            const request = {}
+            request.message = document.querySelector('div.alert.alert-danger').innerText
+            if (request.message.includes('already voted')) {
                 chrome.runtime.sendMessage({later: true})
                 return
             }
-            chrome.runtime.sendMessage({message: document.querySelector('div.alert.alert-danger').textContent})
+            if (request.message.includes('There was a problem during the vote, please try again')) {
+                request.ignoreReport = true
+            }
+            chrome.runtime.sendMessage(request)
             // return
         }
     } catch (e) {
