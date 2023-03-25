515255805a93e5b8b68679a34874f42215a55c82
Serega007 <ermaksochi@gmail.com>
2023-03-25 12:00:51 +0300

    wargm.ru не отправляем отчёт если требуется подтвердить обработку персональных данных

--- scripts/wargm.js
+++ scripts/wargm.js
@@ -78,7 +78,7 @@ const timer = setInterval(async ()=>{
             } else if (request.message === 'Успешно' || request.message.includes('In process...')) {
                 // None
             } else {
-                if (request.message.includes('Сервис временно недоступен') || request.message.includes('Страница устарела') || request.message === 'Ошибка' || (request.message.includes('Запрос отклонен') && request.message.includes('Поступило слишком много запросов'))) {
+                if (request.message.includes('Сервис временно недоступен') || request.message.includes('Страница устарела') || request.message === 'Ошибка' || (request.message.includes('Запрос отклонен') && request.message.includes('Поступило слишком много запросов')) || request.message.includes('Важные документы')) {
                     request.ignoreReport = true
                 }
                 clearInterval(timer)
