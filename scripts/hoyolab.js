cefb4875974f1a7d005b01ad26a3f4281c1ec592
Serega007 <ermaksochi@gmail.com>
2023-06-28 14:01:22 +0300

    hoyolab.com закрываем всякие рекламные модалки

--- scripts/hoyolab.js
+++ scripts/hoyolab.js
@@ -1,7 +1,17 @@
 async function vote(first) {
     if (!first) return
 
-    await wait(5000) // TODO дикий костыль ожидающий загрузки страницы
+    // TODO дикий костыль ожидающий загрузки страницы
+    // была попытка сделать адекватное ожидание станицы
+    // но в html никакой нет информации что страница ещё не полностью загрузилась
+    // можно конечно было сделать ожидание ответа fetch (xhr) запроса но и там проблемы
+    // сайт делает таких запросов несколько
+    // и также genshin и honkai делают эти запросы разные (хоть они и схожие)
+    // также имеет место быть проблема если пользователь не авторизован
+    // если нет авторизации то и fetch (xhr) запросов на загрузку сколько отметков выполнил пользователь
+    // что вставляет ещё больше палок в колёса
+    // поэтому тут такое тупое await wait()
+    await wait(5000)
 
     const project = await getProject('HoYoLAB')
 
@@ -25,12 +35,20 @@ async function vote(first) {
 }
 
 const timer = setInterval(() => {
-    const message = document.querySelector('div.sign-wrapper')?.innerText || document.querySelector('div.van-toast__text')?.innerText || document.querySelector('div.m-dialog-body')?.innerText
+    const message = document.querySelector('div.sign-wrapper')?.innerText ||
+        document.querySelector('div.van-toast__text')?.innerText ||
+        document.querySelector('div.m-dialog-body')?.innerText ||
+        document.querySelector('div[class*="sign-guide"]')?.innerText
     if (message && message.length > 3) {
-        clearInterval(timer)
         if (message.includes('checked in today')) {
+            clearInterval(timer)
             chrome.runtime.sendMessage({successfully: true})
+        } else if (message.includes('You may set up check-in notifications') || message.includes('You can set up check-in reminders')) {
+            // None
+            document.querySelector('div[class*="dialog-close"]')?.click()
+            document.querySelector('span[class*="guide-close"]')?.click()
         } else {
+            clearInterval(timer)
             chrome.runtime.sendMessage({message})
         }
     }
